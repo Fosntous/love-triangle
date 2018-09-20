e33c0f7bfa094a3d4071c0d53a7fa7876c737066
Fosntous <43320126+Fosntous@users.noreply.github.com>
2018-09-20 19:28:30 +0300

    1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  c = 0;
+  for (i = 0;i < preferences.length;i++) {
+    if(preferences[preferences[preferences[i-1]-1]-1]==i)c++;
+  }
+  return Math.round(c/3); 
 };
